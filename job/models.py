e59d55144f30e512c9c0a449b2f856b1aeb6ca77
tnttoutou <toufiktnt@hotmail.fr>
2021-10-19 15:06:58 +0200

    add app for job

--- job/models.py
+++ job/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+JOB_TYPE=(
+      ('full time','full time'),
+      ('part time','part time'),
+)
+
+class job(models.Model):#tabel
+    title=models.CharField(max_length=100)#colune
+    #location
+
+    job_type=models.CharField(max_length=15 , choices=JOB_TYPE)
+
+    description=models.TextField(max_length=1000)
+    published_at=models.DateField(auto_now=True)
+    vacancy=models.IntegerField(default=1)
+    salary=models.IntegerField(default=0)
+    experience=models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.title
---
+++ outline.md
@@ -0,0 +1,30 @@
+job:
+ -title
+ -location
+ -job_type
+ -description
+ -published at
+ -vacancy
+ -salary
+ -category
+ experionce
+
+ apply job
+ post job
+
+blog:
+ -title
+ -description
+ -created at
+ -category
+ -tags
+ -author
+
+ -search
+ -comment
+ -recent posts
+
+contact:
+  -home
+
+ login:
